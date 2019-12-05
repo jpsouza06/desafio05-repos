2925f899966d827ca7337248c1b4f454ffb17e02
Joao Paulo <joaopaulosc06@gmail.com>
2019-12-04 21:48:47 -0300

    Commit inicial

---
+++ src/pages/Main/index.js
@@ -0,0 +1,120 @@
+/* eslint-disable no-throw-literal */
+import React, { Component } from 'react';
+import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+
+import api from '../../services/api';
+
+import Container from '../../components/Container';
+import { Form, SubmitButton, List, Error, Input } from './styles';
+
+export default class Main extends Component {
+  state = {
+    newRepo: '',
+    repositories: [],
+    loading: false,
+    valid: true,
+    errorMessage: '',
+  };
+
+  // Carregar os dados do localStorage
+  componentDidMount() {
+    const repositories = localStorage.getItem('repositories');
+
+    if (repositories) {
+      this.setState({ repositories: JSON.parse(repositories) });
+    }
+  }
+
+  // Salvar os dados do localStorage
+  componentDidUpdate(_, prevState) {
+    const { repositories } = this.state;
+
+    if (prevState.repositories !== repositories) {
+      localStorage.setItem('repositories', JSON.stringify(repositories));
+    }
+  }
+
+  handleInputChange = e => {
+    this.setState({ newRepo: e.target.value });
+  };
+
+  handleSubmit = async e => {
+    e.preventDefault();
+
+    this.setState({ loading: true });
+
+    try {
+      const { newRepo, repositories } = this.state;
+
+      const hasRepo = repositories.find(
+        rep => rep.name.toLowerCase() === newRepo
+      );
+
+      if (hasRepo) throw 'Repositório duplicado.';
+
+      const response = await api.get(`/repos/${newRepo}`);
+
+      const data = {
+        name: response.data.full_name,
+      };
+
+      this.setState({
+        repositories: [...repositories, data],
+        newRepo: '',
+        errorMessage: '',
+        valid: true,
+      });
+    } catch (error) {
+      let message = '';
+      if (error !== 'Repositório duplicado.')
+        message = 'Repositório inexistente.';
+      else message = error;
+      this.setState({ newRepo: '', errorMessage: message, valid: false });
+    } finally {
+      this.setState({ loading: false });
+    }
+  };
+
+  render() {
+    const { newRepo, repositories, loading, valid, errorMessage } = this.state;
+
+    return (
+      <Container>
+        <h1>
+          <FaGithubAlt />
+          Repositórios
+        </h1>
+
+        <Form onSubmit={this.handleSubmit}>
+          <Input
+            type="text"
+            placeholder="Adicionar repositório"
+            valid={!valid}
+            value={newRepo}
+            onChange={this.handleInputChange}
+          />
+
+          <SubmitButton loading={loading ? 1 : 0}>
+            {loading ? (
+              <FaSpinner color="#FFF" size={14} />
+            ) : (
+              <FaPlus color="#FFF" size={14} />
+            )}
+          </SubmitButton>
+        </Form>
+        {valid ? <></> : <Error>{errorMessage}</Error>}
+        <List>
+          {repositories.map(repository => (
+            <li key={repository.name}>
+              <span>{repository.name}</span>
+              <Link to={`/repository/${encodeURIComponent(repository.name)}`}>
+                Detalhes
+              </Link>
+            </li>
+          ))}
+        </List>
+      </Container>
+    );
+  }
+}
